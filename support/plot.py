c7f109f8609b15fd5a93fe070dbf5a779760afee
Philipp Mascha <Philipp.Mascha@ams-osram.com>
2024-11-16 15:16:20 +0100

    Added Übung 3 and filters

--- support/plot.py
+++ support/plot.py
@@ -1,5 +1,6 @@
 import numpy as np
 from IPython.display import display, Math, Latex
+import matplotlib.pyplot as plt
 
 def draw_matrix(mat: np.ndarray) -> str:
     if mat.size == 1:
@@ -14,6 +15,16 @@ def draw_matrix(mat: np.ndarray) -> str:
 
 def plot_tensor(tensor: np.ndarray):
     display(Math(draw_matrix(tensor)))
+
+def show_images(imgs, names = None):
+    fig, axes = plt.subplots(1, len(imgs))
+    fig.set_size_inches(len(imgs) * 5, 5)
+    if not names:
+        names = [''] * len(imgs)
+    for img, ax, name in zip(imgs, axes, names):
+        ax.imshow(img, cmap='grey')
+        ax.title.set_text(name)
+    plt.show()
 
 def main():
     print(draw_matrix(np.ones([3,4,2])))
